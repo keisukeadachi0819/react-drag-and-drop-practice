2bf578360c5476195aaa9df0054d8cd87e0ebed4
keisukeadachi0819 <92622310+keisukeadachi0819@users.noreply.github.com>
2022-06-22 18:03:42 +0900

    レイアウト整形

--- src/App.js
+++ src/App.js
@@ -5,30 +5,63 @@ import './App.css';
 
 function App() {
   return (
-    <div>
-      <h1>Drag and Drop</h1>
-      <DragDropContext>
-        <Droppable droppableId='droppable'>
-          {(provided) => (
-            <div 
-              ref={provided.innerRef}
-              {...provided.droppableProps}
-            >
-              <Draggable draggableId='draggable' index={0}>
+    <div className='container'>
+      <div className='inner'>
+        <div className='content'>
+          <h1 className='title'>DragDropContext</h1>
+          <div className='card_list'>
+            <DragDropContext>
+              <Droppable droppableId='droppable'>
                 {(provided) => (
                   <div 
                     ref={provided.innerRef}
-                    {...provided.draggableProps}
-                    {...provided.dragHandleProps}
+                    {...provided.droppableProps}
                   >
-                    item0
+                    <Draggable draggableId='draggable-1' index={1}>
+                      {(provided) => (
+                        <div
+                          className='card'
+                          ref={provided.innerRef}
+                          {...provided.draggableProps}
+                          {...provided.dragHandleProps}
+                        >
+                          item1
+                        </div>
+                      )}
+                    </Draggable>
+
+                    <Draggable draggableId='draggable-2' index={2}>
+                      {(provided) => (
+                        <div
+                          className='card'
+                          ref={provided.innerRef}
+                          {...provided.draggableProps}
+                          {...provided.dragHandleProps}
+                        >
+                          item2
+                        </div>
+                      )}
+                    </Draggable>
+
+                    <Draggable draggableId='draggable-3' index={3}>
+                      {(provided) => (
+                        <div
+                          className='card'
+                          ref={provided.innerRef}
+                          {...provided.draggableProps}
+                          {...provided.dragHandleProps}
+                        >
+                          item3
+                        </div>
+                      )}
+                    </Draggable>
                   </div>
                 )}
-              </Draggable>
-            </div>
-          )}
-        </Droppable>
-      </DragDropContext>
+              </Droppable>
+            </DragDropContext>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
