512ac26d0ffd8cd6464d1fd898f1d107b87d6e92
keisukeadachi0819 <92622310+keisukeadachi0819@users.noreply.github.com>
2022-06-22 18:50:27 +0900

    ドラッグ＆ドロップでカードの入れ替え

--- src/App.js
+++ src/App.js
@@ -1,60 +1,56 @@
+import { useState } from 'react';
 import { Droppable } from 'react-beautiful-dnd';
 import { Draggable } from 'react-beautiful-dnd';
 import { DragDropContext } from 'react-beautiful-dnd';
 import './App.css';
 
 function App() {
+  const [cards, setCard] = useState([
+    { id: 1, title: "item0" },
+    { id: 2, title: "item1" },
+    { id: 3, title: "item2" }
+  ]);
+
+  const onDragEnd = (e) => {
+    // console.log(e);
+    // console.log(e.source.index);
+    // console.log(e.destination.index);
+
+    // 移動するカードのインデックスを格納
+    const removed = cards.splice(e.source.index, 1);
+    console.log(removed);
+    // 移動後に配置する場所に追加
+    cards.splice(e.destination.index, 0, removed[0]);
+  };
+
   return (
     <div className='container'>
       <div className='inner'>
         <div className='content'>
           <h1 className='title'>DragDropContext</h1>
           <div className='card_list'>
-            <DragDropContext>
+            <DragDropContext onDragEnd={onDragEnd}>
               <Droppable droppableId='droppable'>
                 {(provided) => (
                   <div 
                     ref={provided.innerRef}
                     {...provided.droppableProps}
                   >
-                    <Draggable draggableId='draggable-1' index={1}>
-                      {(provided) => (
-                        <div
-                          className='card'
-                          ref={provided.innerRef}
-                          {...provided.draggableProps}
-                          {...provided.dragHandleProps}
-                        >
-                          item1
-                        </div>
-                      )}
-                    </Draggable>
-
-                    <Draggable draggableId='draggable-2' index={2}>
-                      {(provided) => (
-                        <div
-                          className='card'
-                          ref={provided.innerRef}
-                          {...provided.draggableProps}
-                          {...provided.dragHandleProps}
-                        >
-                          item2
-                        </div>
-                      )}
-                    </Draggable>
-
-                    <Draggable draggableId='draggable-3' index={3}>
-                      {(provided) => (
-                        <div
-                          className='card'
-                          ref={provided.innerRef}
-                          {...provided.draggableProps}
-                          {...provided.dragHandleProps}
-                        >
-                          item3
-                        </div>
-                      )}
-                    </Draggable>
+                    {cards.map((card, index) => (
+                      <Draggable draggableId={card.title} index={index} key={card.id}>
+                        {(provided) => (
+                          <div
+                            className='card'
+                            ref={provided.innerRef}
+                            {...provided.draggableProps}
+                            {...provided.dragHandleProps}
+                          >
+                            {card.title}
+                          </div>
+                        )}
+                      </Draggable>
+                    ))}
+                    {provided.placeholder}
                   </div>
                 )}
               </Droppable>
